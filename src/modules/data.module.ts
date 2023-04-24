b494cd97b9e459aada978f48b54effc64ec898bb
ghazi-mkhinini <ghazimkini@gmail.com>
2023-04-24 19:23:31 +0100

    Added data Module definition

---
+++ src/modules/data.module.ts
@@ -0,0 +1,10 @@
+import { Module } from "@nestjs/common";
+import { requestController } from "./data.controller";
+import { dataService } from "./data.service";
+
+@Module({
+  imports: [],
+  controllers: [requestController],
+  providers: [dataService],
+})
+export class RequestModule {}
